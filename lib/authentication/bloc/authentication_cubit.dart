3f2c76c0615932ec1cca1f53bb44035a833543b1
Carlos MZ <digihazard18@gmail.com>
2020-09-26 22:27:42 -0500

    Agregado configurar nickname

--- lib/authentication/bloc/authentication_cubit.dart
+++ lib/authentication/bloc/authentication_cubit.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebaseAuthPackage;
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 import 'package:just_talk/models/user.dart';
 import 'package:just_talk/services/authentication_service.dart';
 import 'package:meta/meta.dart';
@@ -36,7 +34,8 @@ class AuthenticationCubit extends Cubit<AuthenticationState> {
   }
 
   void logWithFacebook() async {
-    await _authenticationService.logInWithCredentials(await _authenticationService.logInWithFacebook());
+    await _authenticationService
+        .logInWithCredentials(await _authenticationService.logInWithFacebook());
   }
 
   void mapAuthenticationUserChangedToState(AuthenticationUserChanged event) {
