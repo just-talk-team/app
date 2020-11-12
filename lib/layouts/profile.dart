a12b6b657353dc715f5bf162c94646a7d3be6b23
4bel4ven <abl.avn@gmail.com>
2020-11-11 22:54:47 -0500

    Update profile.dart

--- lib/layouts/profile.dart
+++ lib/layouts/profile.dart
@@ -1,35 +1,21 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:just_talk/authentication/bloc/authentication_cubit.dart';
+import 'package:just_talk/models/user_info.dart';
 import 'package:just_talk/models/user_profile.dart';
 import 'package:just_talk/services/user_service.dart';
-import 'package:just_talk/models/user.dart';
 
 class Profile extends StatefulWidget {
+  Profile(this.userId);
+  final String userId;
   @override
-  _Profile createState() => _Profile();
+  _ProfileState createState() => _ProfileState();
 }
 
-class _Profile extends State<Profile> {
-  List<String> arrayTest = [
-    'upc.edu.pe',
-    'cibertec.edu.pe',
-    'googe.com.pe',
-    'upc.edu.pe',
-    'cibertec.edu.pe',
-    'googe.com.pe',
-    'upc.edu.pe',
-    'cibertec.edu.pe',
-    'googe.com.pe',
-    'upc.edu.pe',
-    'cibertec.edu.pe',
-    'googe.com.pe',
-    'upc.edu.pe',
-    'cibertec.edu.pe',
-    'googe.com.pe',
-  ];
+class _ProfileState extends State<Profile> {
+  UserProfile userProfile;
+  UserInfo userInfo;
+
   List<String> topicsHear = [
     'Clásicas de cachimbos',
     'Tips para examentes',
@@ -68,220 +54,200 @@ class _Profile extends State<Profile> {
     'Teorias de conspiracion',
     'Among US'
   ];
-  Future<UserProfile> getUserInfo(BuildContext context) async {
+
+  Future<bool> getUser(BuildContext context) async {
     UserService userService = UserService();
-    User user = BlocProvider.of<AuthenticationCubit>(context).state.user;
-    return await userService.getUserInfoProfile(user.id);
+    userInfo = await userService.getUser(widget.userId, false, false);
+    userProfile = await userService.getUserInfoProfile(widget.userId);
+    if (userInfo != null && userProfile != null) {
+      return true;
+    }
+    return false;
   }
 
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Stack(
-          children: [
-            Container(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, '/login');
-                },
-                child: Container(
-                  child: Icon(Icons.keyboard_arrow_left,
-                      size: 40, color: Color(0xff666666)),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Column(
-                children: [
-                  SizedBox(height: 10),
-                  Text(
-                    'Perfil',
-                    style: TextStyle(
-                        color: Color(0xff666666),
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-          ],
+        title: Text(
+          'Mi perfil',
         ),
+        actions: <Widget>[],
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 50),
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 30, 0),
-                        width: 80,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                'https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture.jpg',
-                              ),
-                              fit: BoxFit.fill),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: FutureBuilder(
+          future: getUser(context),
+          builder: (context, AsyncSnapshot<bool> flagInfo) {
+          
+
+            if (flagInfo.hasData && flagInfo.data) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Row(
                         children: [
-                          Text(
-                            'Alexa',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 25),
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 30),
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundColor: Colors.grey,
+                              backgroundImage: NetworkImage(
+                                userInfo.photo,
+                              ),
+                            ),
                           ),
-                          Text(
-                            'Mujer',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            '18 años',
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 0, 0, 3),
+                                child: Text(
+                                 userInfo.nickname,
+                                  style: Theme.of(context).textTheme.headline6,
+                                ),
+                              ),
+                              Text(describeEnum(userInfo.gender),
+                                  style: Theme.of(context).textTheme.caption),
+                              Text("${userInfo.age} años",
+                                  style: Theme.of(context).textTheme.caption),
+                            ],
                           )
                         ],
-                      )
-                    ],
-                  ),
-                ),
-                //Segments
-                Container(
-                  height: 80,
-                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: Wrap(
-                        spacing: 6.0,
-                        runSpacing: 6.0,
-                        children: List<Widget>.generate(arrayTest.length,
-                            (int index) {
-                          return Chip(
-                            label: Text(arrayTest[index]),
-                          );
-                        }),
                       ),
                     ),
-                  ),
-                )
-              ],
-            ),
-            //Insignias
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 50),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.all(2),
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                border: Border.all(
-                                    width: 2, color: Color(0xffb3a407))),
-                            child: Icon(
-                              Icons.hearing,
-                              size: 30,
-                              color: Color(0xffb3a407),
-                            ),
+                    Container(
+                      height: 80,
+                      margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Wrap(
+                            spacing: 6.0,
+                            runSpacing: 6.0,
+                            children: List<Widget>.generate(userProfile.segments.length,
+                                (int index) {
+                              return Chip(
+                                label: Text(userProfile.segments[index]),
+                              );
+                            }),
                           ),
-                          AutoSizeText(
-                            'Buen oyente',
-                            maxLines: 2,
-                            style: TextStyle(color: Color(0xffb3a407)),
-                          )
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 50),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.all(2),
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(100),
+                                        border: Border.all(
+                                            width: 2,
+                                            color: Color(0xffb3a407))),
+                                    child: Icon(
+                                      Icons.hearing,
+                                      size: 30,
+                                      color: Color(0xffb3a407),
+                                    ),
+                                  ),
+                                  AutoSizeText(
+                                    'Buen oyente',
+                                    maxLines: 2,
+                                    style: TextStyle(color: Color(0xffb3a407)),
+                                  )
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.all(2),
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(100),
+                                        border: Border.all(
+                                            width: 2,
+                                            color: Color(0xffb3a407))),
+                                    child: Icon(
+                                      Icons.mood,
+                                      size: 30,
+                                      color: Color(0xffb3a407),
+                                    ),
+                                  ),
+                                  AutoSizeText(
+                                    'Buen conversador',
+                                    maxLines: 2,
+                                    style: TextStyle(color: Color(0xffb3a407)),
+                                  )
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.all(2),
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(100),
+                                        border: Border.all(
+                                            width: 2,
+                                            color: Color(0xffb3a407))),
+                                    child: Icon(
+                                      Icons.sentiment_very_satisfied,
+                                      size: 30,
+                                      color: Color(0xffb3a407),
+                                    ),
+                                  ),
+                                  AutoSizeText('Divertido',
+                                      maxLines: 2,
+                                      style:
+                                          TextStyle(color: Color(0xffb3a407)))
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10)
                         ],
                       ),
-                      Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.all(2),
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                border: Border.all(
-                                    width: 2, color: Color(0xffb3a407))),
-                            child: Icon(
-                              Icons.mood,
-                              size: 30,
-                              color: Color(0xffb3a407),
+                    ),
+                    //TopicsHear
+                    Expanded(
+                      child: Container(
+                          margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: Wrap(
+                              spacing: 6.0,
+                              runSpacing: 6.0,
+                              children: List<Widget>.generate(userProfile.topicsHear.length,
+                                  (int index) {
+                                return Chip(
+                                  label: Text(userProfile.topicsHear[index]),
+                                );
+                              }),
                             ),
-                          ),
-                          AutoSizeText(
-                            'Buen conversador',
-                            maxLines: 2,
-                            style: TextStyle(color: Color(0xffb3a407)),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.all(2),
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                border: Border.all(
-                                    width: 2, color: Color(0xffb3a407))),
-                            child: Icon(
-                              Icons.sentiment_very_satisfied,
-                              size: 30,
-                              color: Color(0xffb3a407),
-                            ),
-                          ),
-                          AutoSizeText(
-                            'Divertido',
-                              maxLines: 2,
-                              style: TextStyle(color: Color(0xffb3a407)))
-                        ],
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10)
-                ],
-              ),
-            ),
-            //TopicsHear
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Wrap(
-                    spacing: 6.0,
-                    runSpacing: 6.0,
-                    children:
-                        List<Widget>.generate(topicsHear.length, (int index) {
-                      return Chip(
-                        label: Text(topicsHear[index]),
-                      );
-                    }),
-                  ),
+                          )),
+                    )
+                  ],
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+              );
+            }
+
+            return Center(child: CircularProgressIndicator());
+          }),
     );
   }
 }
