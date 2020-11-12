5d4787fb8cd7279235d4a2f14710764438d22363
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2020-11-12 04:04:29 -0500

    Se corrige interface preference y se verifica la ausencia de segmento ALL

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 
-const List<Tuple2<String,IconData>> badgets = [
-      Tuple2('Buen oyente', Icons.hearing),
-      Tuple2('Buen conversador', Icons.mood),
-      Tuple2('Divertido', Icons.mood)
+const List<Tuple2<String, IconData>> badgets = [
+  Tuple2('Buen oyente', Icons.hearing),
+  Tuple2('Buen conversador', Icons.mood),
+  Tuple2('Divertido', Icons.sentiment_very_satisfied_rounded)
 ];
