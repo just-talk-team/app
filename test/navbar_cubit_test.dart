2e07944e9c9c5e8056af2c5c84ae2b2e9fc53ff6
4bel4ven <abl.avn@gmail.com>
2020-11-14 01:06:37 -0500

    Update navbar_cubit_test.dart

--- test/navbar_cubit_test.dart
+++ test/navbar_cubit_test.dart
@@ -1,15 +1,14 @@
-  
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:just_talk/bloc/navbar_cubit.dart';
 
 void main() {
-  test('El estado inicial es HomeState', () {
+  test('The initial state is HomeState', () {
     final navbarCubit = NavbarCubit();
     expect(navbarCubit.state, HomeState());
     navbarCubit.close();
   });
-  
+
   group('NavbarCubit state', () {
     blocTest<NavbarCubit, NavBarState>(
       'NavbarCubit navigation to home',
